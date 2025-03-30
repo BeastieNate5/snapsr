d72b69aca9d6aca284a7930758eabdb0ed524826
BeastieNate5 <nhagan345@gmail.com>
2025-03-30 10:26:57 -0400

    Refactored Path construction

--- src/snaps.rs
+++ src/snaps.rs
@@ -43,9 +43,21 @@ fn get_all_snaps() -> Vec<String> {
 }
 
 pub fn take_snap(snap_name: String) {
-    let snap = read_snap_config(get_snap_config_dir() + "/snaps.jsonc");
-    let snap_dir = get_snaps_dir() + "/" + &snap_name.as_str(); 
-    let snap_dir = path::Path::new(&snap_dir);
+    let snap = get_snap_config_dir();
+    let snap = path::Path::new(&snap).join("snaps.jsonc");
+    
+    let snap = match snap.to_str() {
+        Some(txt) => txt,
+        None => {
+            println!("Failed to read Snaps config dir");
+            return
+        }
+    };
+
+    let snap = read_snap_config(snap.to_string());
+
+    let snap_dir = get_snaps_dir();
+    let snap_dir = path::Path::new(&snap_dir).join(&snap_name);
 
     if let Err(_) = fs::create_dir_all(&snap_dir) {
         eprintln!("Failed to create snap directory");
@@ -66,8 +78,7 @@ pub fn take_snap(snap_name: String) {
         }
 
         for item in config.items {
-            let full_path = format!("{}/{}", config.path, item);
-            let full_path = path::Path::new(&full_path);
+            let full_path = path::Path::new(&config.path).join(&item);
 
             if let Err(_) = fs::copy(full_path, module_dir.join(&item)) {
                 println!("Failed to copy {item}, skipping");
@@ -88,15 +99,26 @@ pub fn transfer_snap(snap_name: String) {
         return;
     }
 
-    let snap_dir = get_snaps_dir() + "/" + &snap_name;
-    let snap_config_path = snap_dir.clone() + "/snap.jsonc";
-    let snap = read_snap_config(snap_config_path);
+    let snap_dir = get_snaps_dir();
+    let snap_dir = path::Path::new(&snap_dir).join(&snap_name);
+
+    let snap_config_path = path::Path::new(&snap_dir).join("snap.jsonc");
+    
+    let snap_config_path = match snap_config_path.to_str() {
+        Some(txt) => txt,
+        None => {
+            eprintln!("Failed to read snap {snap_name} config");
+            return;
+        }
+    };
+
+    let snap = read_snap_config(snap_config_path.to_string());
 
     for (module_name, module) in snap {
         
         for item in module.items {
-            let dst_path = module.path.clone() + "/" + &item;
-            let src_path = snap_dir.clone() + "/" + &module_name + "/" + &item;
+            let dst_path = path::Path::new(&module.path).join(&item);
+            let src_path = path::Path::new(&snap_dir).join(&module_name).join(&item);
             
             if let Err(_) = fs::copy(src_path, dst_path) {
                 println!("Failed to transfer item {item} from module {module_name}");
@@ -119,9 +141,10 @@ pub fn delete_snap(snap: String) {
     }
 
     let snap_dir = get_snaps_dir();
-    let snap_dir = snap_dir + "/" +&snap;
-    if let Err(e) = fs::remove_dir_all(snap_dir) {
-        eprintln!("Failed to delete snap, {e}");
+    let snap_dir = path::Path::new(&snap_dir).join(&snap);
+
+    if let Err(_) = fs::remove_dir_all(snap_dir) {
+        eprintln!("Failed to delete snap");
         return;
     }
 
