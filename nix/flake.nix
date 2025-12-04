2781f7efdd98312b410ca00444e8f354d9939df2
0x2B <nhagan345@gmail.com>
2025-12-04 13:56:23 -0500

    Added nix flake

---
+++ nix/flake.nix
@@ -0,0 +1,15 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    
+    outputs = { nixpkgs }: 
+    let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+        packages.x86_64-linux.default = pkgs.callPackage ./default.nix {};
+    };
+}
